8a9155e37a0d47fcfc84cd3f0f173f73bd4ddf36
Ben <ben.shamas@live.com>
2022-11-17 13:37:39 -0500

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
           I'm a Full Stack Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          Currently obssesed with building and designing responsive web applications with great user experiences.
+          Currently interested in building and designing responsive web applications with great user experiences.
         </p>
         <div>
             <Link to='work' smooth={true} duration={500}>
