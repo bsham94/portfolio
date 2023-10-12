7bc8c5f4d4d0d08309f5cdde19880f9d9f71e97a
Ben Shamas <32513784+bsham94@users.noreply.github.com>
2023-10-12 12:36:10 -0400

    added restaurant work example

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,34 +1,71 @@
-import React from 'react'
-import Terra from '../assets/terra.jpg'
-import Twig from '../assets/twig.jpg'
-import Memories from '../assets/memories.jpg'
+import React from "react";
+import Terra from "../assets/terra.jpg";
+import Twig from "../assets/twig.jpg";
+import Memories from "../assets/memories.jpg";
+import Restaurant from "../assets/restaurant.png";
 
 const work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
           </p>
-          <p className='py-6'>Check out some of my recent work</p>
+          <p className="py-6">Check out some of my recent work</p>
         </div>
 
         {/* Container */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-        {/* Grid Item */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${Memories})`,"background-size": "100% 100%" }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            style={{
+              backgroundImage: `url(${Restaurant})`,
+              "background-size": "100% 100%",
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100 text-center'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
+            <div className="opacity-0 group-hover:opacity-100 text-center">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Restaurant
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/bsham94/restaurant-website"
+                  target="blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+                <a href="https://www.littledings.ca/" target="blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{
+              backgroundImage: `url(${Memories})`,
+              "background-size": "100% 100%",
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 text-center">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 Social Media App
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://github.com/bsham94/social-media-app' target="blank">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://github.com/bsham94/social-media-app"
+                  target="blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -40,19 +77,22 @@ const work = () => {
               </div>
             </div>
           </div>
-            {/* Grid Item */}
+          {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${Terra})`,"background-size": "100% 100%"}}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-contain'
+            style={{
+              backgroundImage: `url(${Terra})`,
+              "background-size": "100% 100%",
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-contain"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100 text-center'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                 TERRA
+            <div className="opacity-0 group-hover:opacity-100 text-center">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                TERRA
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://github.com/bsham94/terra' target="blank">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              <div className="pt-8 text-center">
+                <a href="https://github.com/bsham94/terra" target="blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -60,17 +100,20 @@ const work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${Twig})`,"background-size": "100% 100%" }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-cover'
+            style={{
+              backgroundImage: `url(${Twig})`,
+              "background-size": "100% 100%",
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-cover"
           >
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100 text-center'>
-              <span className='text-2xl font-bold text-white tracking-wider '>
-              Twig
+            <div className="opacity-0 group-hover:opacity-100 text-center">
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                Twig
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://github.com/bsham94/twig' target="blank">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              <div className="pt-8 text-center">
+                <a href="https://github.com/bsham94/twig" target="blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -80,7 +123,7 @@ const work = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default work
+export default work;
